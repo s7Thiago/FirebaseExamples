f80837bd7c95e4f12407aa14adb2edfe971273cb
s7thiago <thyagosousasilva@gmail.com>
2018-09-02 23:08:48 -0300

    Adicionando exemplo de animação com elementos compartilhados entre telas

---
+++ app/src/main/java/br/com/thiagosousa/firebaseexamples/activitys/SharedAnimationExample.java
@@ -0,0 +1,15 @@
+package br.com.thiagosousa.firebaseexamples.activitys;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.com.thiagosousa.firebaseexamples.R;
+
+public class SharedAnimationExample extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shared_animation_example);
+    }
+}
