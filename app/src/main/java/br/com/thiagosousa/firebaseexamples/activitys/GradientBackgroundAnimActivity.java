c0c823f160090d51f6a5962c97119e9f1aae9a41
s7thiago <thyagosousasilva@gmail.com>
2018-09-02 23:07:32 -0300

    Adicionando tela com exemplo de animação em gradiente

---
+++ app/src/main/java/br/com/thiagosousa/firebaseexamples/activitys/GradientBackgroundAnimActivity.java
@@ -0,0 +1,55 @@
+package br.com.thiagosousa.firebaseexamples.activitys;
+
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.constraint.ConstraintLayout;
+import android.util.Log;
+import android.widget.LinearLayout;
+
+import br.com.thiagosousa.firebaseexamples.R;
+import br.com.thiagosousa.firebaseexamples.useful.AuthDataBaseActivity;
+
+public class GradientBackgroundAnimActivity extends AuthDataBaseActivity {
+
+    private static final String GRADIENTBACKGROUNDANIMACTIVITYTAG = "Activity_anim event";
+
+    LinearLayout layout;
+    ConstraintLayout constraintLayout;
+    AnimationDrawable animationDrawable;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gradient_background_anim);
+        initViews(true);
+        animBackbround(true);
+    }
+
+    public void initViews(boolean init) {
+        if (init) {
+            layout = findViewById(R.id.activity_gradient_background_anim_linearlayout);
+            constraintLayout = findViewById(R.id.constraint_background_anim_background);
+        } else {
+
+        }
+    }
+
+    //    animBackbround(boolean anim) <- se init = true, o fundo desta tela é animado com um efeito gradiente
+    public void animBackbround(boolean anim) {
+
+        if (anim) {
+
+            Log.w(GRADIENTBACKGROUNDANIMACTIVITYTAG, "Animação gradiente ativa");
+
+            animationDrawable = (AnimationDrawable) layout.getBackground();
+            animationDrawable.setEnterFadeDuration(3000);
+            animationDrawable.setExitFadeDuration(3000);
+            animationDrawable.start();
+        } else {
+            Log.w(GRADIENTBACKGROUNDANIMACTIVITYTAG, "Background anim desactivated");
+        }
+
+    }
+}
