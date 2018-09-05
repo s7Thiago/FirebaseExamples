78699e20f0c318c58c936a28c45a5054ed2cbed2
s7thiago <thyagosousasilva@gmail.com>
2018-09-05 07:44:21 -0300

    Adicionado exemplo de imagem vetorizada em uso

---
+++ app/src/main/java/br/com/thiagosousa/firebaseexamples/activitys/MoveFunLogoSvgActivity.java
@@ -0,0 +1,15 @@
+package br.com.thiagosousa.firebaseexamples.activitys;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.com.thiagosousa.firebaseexamples.R;
+
+public class MoveFunLogoSvgActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_move_fun_logo_svg);
+    }
+}
