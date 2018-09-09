3e774ceb49add2a8d259382115693224bfaac0e3
s7thiago <thyagosousasilva@gmail.com>
2018-09-09 11:50:49 -0300

    Adicionando código para tela que mostra um exemplo listview implementado com o padrão ViewHolder

---
+++ app/src/main/java/br/com/thiagosousa/firebaseexamples/activitys/CustomListViewViewHolderActivity.java
@@ -0,0 +1,15 @@
+package br.com.thiagosousa.firebaseexamples.activitys;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.com.thiagosousa.firebaseexamples.R;
+
+public class CustomListViewViewHolderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_custom_list_view_view_holder);
+    }
+}
