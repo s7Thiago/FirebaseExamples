e1f5e897240e67371ee10d09793a6bd8dd3066c8
s7Thiago <thyagosousasilva@gmail.com>
2018-10-07 09:35:56 -0300

    Implementando shared animation

--- app/src/main/java/br/com/thiagosousa/firebaseexamples/activitys/CustomListViewViewHolderActivity.java
+++ app/src/main/java/br/com/thiagosousa/firebaseexamples/activitys/CustomListViewViewHolderActivity.java
@@ -1,19 +1,26 @@
 package br.com.thiagosousa.firebaseexamples.activitys;
 
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.support.v4.app.ActivityOptionsCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.Pair;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 
+import br.com.thiagosousa.firebaseexamples.DetailViewHolderActivity;
 import br.com.thiagosousa.firebaseexamples.R;
 import br.com.thiagosousa.firebaseexamples.adapter.AdapterWithViewHolder;
 import br.com.thiagosousa.firebaseexamples.objects.ItemForActivityWithListViewHolder;
 
-public class CustomListViewViewHolderActivity extends AppCompatActivity {
+public class CustomListViewViewHolderActivity extends AppCompatActivity implements AdapterView.OnItemClickListener{
     private static final String ACTIVITYVIEWHOLDERTAG = "ActivityViewHolderList";
 
     private ListView listView;
@@ -38,6 +45,7 @@ public class CustomListViewViewHolderActivity extends AppCompatActivity {
             adapter = new AdapterWithViewHolder(this, itens);
 
             listView.setAdapter(adapter);
+            listView.setOnItemClickListener(this);
 
         } else {
             Log.w(ACTIVITYVIEWHOLDERTAG, "O método configureScreen() está desativade");
@@ -67,5 +75,35 @@ public class CustomListViewViewHolderActivity extends AppCompatActivity {
         }
 
         return itens;
+    }
+
+//    Eventos de clique do listView
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        switch (position){
+            default:
+//                Configurando os pares para a transição entre as telas
+
+                View view1 = view.findViewById(R.id.item_customlistviewholderactivity_imageview);
+                View view2 = view.findViewById(R.id.item_customlistviewholderactivity_title_textview);
+                View view3 = view.findViewById(R.id.item_customlistviewholderactivity_time_textview);
+                View view4 = view.findViewById(R.id.item_customlistviewholderactivity_address_textview);
+
+                Pair<View, String> pair1 = Pair.create(view1, view1.getTransitionName());
+                Pair<View, String> pair2 = Pair.create(view2, view2.getTransitionName());
+                Pair<View, String> pair3 = Pair.create(view3, view3.getTransitionName());
+                Pair<View, String> pair4 = Pair.create(view4, view4.getTransitionName());
+
+//                Configurando o intent com os pares
+                Intent intent = new Intent(getBaseContext(), DetailViewHolderActivity.class);
+                ActivityOptions activityOptions = ActivityOptions.makeSceneTransitionAnimation(this, pair1, pair2, pair3, pair4);
+
+//                Colocando os dados do objeto na posição clicada no intent
+                ItemForActivityWithListViewHolder item = itens.get(position);
+                intent.putExtra(DetailViewHolderActivity.EXTRA_ITEM_OBJECT, item);
+
+                startActivity(intent, activityOptions.toBundle());
+                break;
+        }
     }
 }
