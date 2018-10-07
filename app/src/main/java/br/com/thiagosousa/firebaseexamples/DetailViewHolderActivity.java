8ecfa37e4cba591cd406c0353610f780e9b412f2
s7Thiago <thyagosousasilva@gmail.com>
2018-10-07 09:38:22 -0300

    Adicionando novas telas

---
+++ app/src/main/java/br/com/thiagosousa/firebaseexamples/DetailViewHolderActivity.java
@@ -0,0 +1,64 @@
+package br.com.thiagosousa.firebaseexamples;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import br.com.thiagosousa.firebaseexamples.objects.ItemForActivityWithListViewHolder;
+
+public class DetailViewHolderActivity extends AppCompatActivity {
+
+    private static final String DETAILVIEWHOLDERACTIVITYTAG= "DtailViewHolderActivity";
+    public static final String EXTRA_ITEM_OBJECT = "ITEM_OBJECT";
+
+    private ImageView image;
+    private TextView title;
+    private TextView time;
+    private TextView addressDescriptio;
+    private ItemForActivityWithListViewHolder item;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_view_holder);
+        initViews(true);
+        configureScreen(true);
+    }
+
+    private void configureScreen(boolean configure) {
+        if (configure) {
+
+            //Coloca os dados recebidos via intent na tela
+            preencherTela();
+
+        } else {
+            Log.w(DETAILVIEWHOLDERACTIVITYTAG, "O método configureScreen() está desativado");
+        }
+    }
+
+    private void initViews(boolean init) {
+        if (init) {
+
+            image = findViewById(R.id.detail_viewholderactivity_image);
+            title = findViewById(R.id.detail_viewholderactivity_text_title);
+            time = findViewById(R.id.detail_viewholderactivity_text_time);
+            addressDescriptio = findViewById(R.id.detail_viewholderactivity_text_address_description);
+
+
+        } else {
+            Log.w(DETAILVIEWHOLDERACTIVITYTAG, "O método initViews() está desativado");
+        }
+    }
+
+    public void preencherTela() {
+        item = getIntent().getParcelableExtra(EXTRA_ITEM_OBJECT);
+
+        title.setText(item.getTitle());
+        time.setText(item.getTime());
+        addressDescriptio.setText(getString(R.string.lorem_ipsum_description));
+
+    }
+}
---
+++ app/src/main/java/br/com/thiagosousa/firebaseexamples/activitys/RecyclerViewActivity.java
@@ -0,0 +1,10 @@
+package br.com.thiagosousa.firebaseexamples.activitys;
+
+import br.com.thiagosousa.firebaseexamples.useful.UtilActivity;
+
+public class RecyclerViewActivity extends UtilActivity {
+    private static final String TAG = "RecyclerViewActivity";
+
+
+}
+
